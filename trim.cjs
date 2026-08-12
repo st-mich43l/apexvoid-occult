fb6d5b57c0fe8690d670d5b110cd22fff6f794c5
Michael Winchester <michaeldang.general@gmail.com>
2026-08-12 21:48:54 +0700

    chore: remove obsolete whitespace scratch script

--- trim.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const files = [
-  'src/lib/ziwei/analysis/modules/major-fortune/audit/v0.5-production-shadow/cli/decision-check-production-shadow.ts'
-];
-for (const file of files) {
-  let content = fs.readFileSync(file, 'utf8');
-  content = content.replace(/[ \t]+$/gm, '');
-  fs.writeFileSync(file, content, 'utf8');
-}
