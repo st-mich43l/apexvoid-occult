398064647039c8de186fe4b48e70f00f495b7779
Michael Winchester <michaeldang.general@gmail.com>
2026-08-24 20:19:44 +0700

    feat(ui): use current Annual Axes detail renderer

--- src/components/ziwei/annual-axes/AnnualAxesSection.tsx
+++ src/components/ziwei/annual-axes/AnnualAxesSection.tsx
@@ -6,7 +6,7 @@ import {
   type AnnualAxesResult,
 } from "@/lib/ziwei/analysis/modules/annual-axes";
 import { AnnualAxesRadar } from "./AnnualAxesRadar";
-import { AnnualAxisDetail } from "./AnnualAxisDetail";
+import { AnnualAxisDetailCurrent } from "./AnnualAxisDetailCurrent";
 import { ANNUAL_AXIS_BAND_LABEL_VI, ANNUAL_AXIS_DOMAIN_ORDER, ANNUAL_AXIS_LABEL_VI } from "./labels";
 import "./annual-axes.css";
 
@@ -104,7 +104,7 @@ export function AnnualAxesSection({ chart, school, result }: AnnualAxesSectionPr
 
       {selectedDomain && computed.axes[selectedDomain] ? (
         <div ref={detailRef}>
-          <AnnualAxisDetail
+          <AnnualAxisDetailCurrent
             domain={selectedDomain}
             axis={computed.axes[selectedDomain]}
             onClose={() => setSelectedDomain(null)}
